48e946e75d5a20f329b2701ac47e24caf9ea9079
Olli Hanhimäki <olli.hanhimaki@pinja.com>
2026-04-09 20:51:46 +0300

    LSP has moved to likec4 cli

--- lsp/likec4.lua
+++ lsp/likec4.lua
@@ -1,13 +1,13 @@
 return {
-  -- The command to start the language server.
-  -- This assumes 'likec4-language-server' is in your system's PATH.
-  cmd = { "likec4-language-server", "--stdio" },
+	-- The command to start the language server.
+	-- This assumes 'likec4' is in your system's PATH.
+	cmd = { "likec4", "lsp", "--stdio" },
 
-  -- The filetypes for which this server should be enabled.
-  filetypes = { "likec4" },
+	-- The filetypes for which this server should be enabled.
+	filetypes = { "likec4", "c4" },
 
-  -- Root markers used to find the project root.
-  -- The language server will only attach to a project
-  -- if one of these files exists in an ancestor directory.
-  root_markers = { "likec4.config.json", ".git" },
+	-- Root markers used to find the project root.
+	-- The language server will only attach to a project
+	-- if one of these files exists in an ancestor directory.
+	root_markers = { "likec4.config.json", ".git" },
 }
